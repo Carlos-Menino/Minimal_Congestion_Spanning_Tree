71dea3bfdc3eb21c78b5af882677e968864ff6d7
Carlos-Menino <113457026+Carlos-Menino@users.noreply.github.com>
2025-03-18 13:37:32 +0100

    Update spanning_trees.py
    Comments added and irrelevant methods removed

--- spanning_trees.py
+++ spanning_trees.py
@@ -1,27 +1,32 @@
+#Basic methods to operate with spanning trees
+#A graph is instanced as a list of edges. An edge is instanced as a list [u, v, w] where u is the initial vertex, v is the ending vertex and w is a positive weight
+#Unweighted graphs are treated as graphs with constant weight w = 1
+#At this moment we are not dealing with oriented graphs, thus we are not going to make distinction between [u,v,w] and [v,u,w].
+
 import random as rd
 
-def vertex_degree(G,v):
+def vertex_degree(G,v): #deg(v) for v vertex in G
     value = 0
     for edge in G:
         if edge[0] == v or edge[1] == v:
             value = value + 1
     return value
 
-def weighted_degree(G,v):
+def weighted_degree(G,v): # sum of weights of adjacent edges to a vertex v
     value = 0
     for edge in G:
         if edge[0] == v or edge[1] == v:
             value = value + edge[2]
     return value
 
-def vertex_star(G,v):
+def vertex_star(G,v): #List of adjacent vertices to v
     star = []
     for edge in G:
         if edge[0] == v or edge[1] == v:
             star.append(edge)
     return star   
 
-def vertex_star_vertex(G,v):
+def vertex_star_vertex(G,v): #List of vertices connected to v by a single edge
     star = []
     for edge in G:
         if edge[0] == v:
@@ -30,7 +35,7 @@ def vertex_star_vertex(G,v):
             star.append(edge[0])
     return star 
 
-def graph_as_dictionary(G):
+def graph_as_dictionary(G): #This method allows to change a graph to a dictionary format: G[v] is a list with triples [a,b,c], a is adjacent vertex to v, b is the edge-weight and c=+-1 depending in the orientation of the edge
     n = cardinal(G)
     Dic_G = dict()
     for i in range(n):
@@ -40,7 +45,7 @@ def graph_as_dictionary(G):
         Dic_G[e[1]].append([e[0],e[2],-1])
     return Dic_G
 
-def cardinal(G):
+def cardinal(G): # Gets the number of vertices of a graph defined as a list
     V = []
     cardinal = 0
     for edge in G:
@@ -52,7 +57,7 @@ def cardinal(G):
             cardinal = cardinal + 1
     return cardinal 
 
-def vertex_set(Graph):
+def vertex_set(Graph): # Gets the vertex set of a graph defined as a list
     V = []
     for e in Graph:
         if not(e[0] in V):
@@ -86,27 +91,7 @@ def labeling_tree(T): #assigns a tree orientation to a tree T
     #root = T[0][0]
     return T_label, V_label, V_level
 
-def global_tree(Span_Tree, SubTree):
-    New_Tree = SubTree.copy()
-    Vertices_SubTree = vertex_set(SubTree)
-    Aux_Tree = Span_Tree.copy()
-    while len(Vertices_SubTree)< len(Span_Tree)+1:
-        e = Aux_Tree[0]
-        if e[0] in Vertices_SubTree and not(e[1] in Vertices_SubTree):
-            New_Tree.append(e)
-            Vertices_SubTree.append(e[1])
-            Aux_Tree.remove(e)
-        if e[1] in Vertices_SubTree and not(e[0] in Vertices_SubTree):
-            New_Tree.append(e)
-            Vertices_SubTree.append(e[0])
-            Aux_Tree.remove(e)
-        else:
-            Aux_copy = Aux_Tree.copy()
-            for i in range(len(Aux_Tree)):
-                Aux_Tree[i] = Aux_copy[i-1]
-    return New_Tree
-
-def spanning_tree(G): 
+def spanning_tree(G): # Generates a spanning tree T for a graph G
     T = [G[0]]
     VT = [T[0][0],T[0][1]]
     vertices = cardinal(G)
@@ -128,14 +113,7 @@ def spanning_tree(G):
                 G_copy[i-1] = G_aux[i]       
     return T
 
-def spanning_subtree(T,A):
-    TA = []
-    for e in T:
-        if (e[0] in A) and (e[1] in A):
-            TA.append(e)
-    return TA
-
-def random_spanning_tree(Graph): #puede no generar un árbol tal y como está escrito [CORREGIR]
+def random_spanning_tree(Graph): # Generates a random spanning tree T for a graph G
     Graph = rd.sample(Graph,len(Graph))    
     Tree = [Graph[0]]
     VT = [Tree[0][0],Tree[0][1]]
@@ -158,57 +136,12 @@ def random_spanning_tree(Graph): #puede no generar un árbol tal y como está es
                 G_copy[i] = G_aux[i-1] 
     return Tree
 
-def hypercube_spanning_tree(n):
+def hypercube_spanning_tree(n): #Generates thee symmetric spanning tree of the hypercube
     T = [[0,1,1]]
     for i in range(1,n):
         T_copy = T.copy()
         for e in T_copy:
             T.append([e[0]+2**i,e[1]+2**i,1])
         T.append([0,2**i,1])
-    return T
-
-def good_spanning_tree(G): #A veces no sale árbol generador, revisar
-    G = rd.sample(G,len(G))    
-    V = []
-    for edge in G:
-        if not(edge[0] in V):
-            V.append(edge[0])
-        if not(edge[1] in V):
-            V.append(edge[1])
-    degrees = []
-    for v in V:
-        degrees.append(vertex_degree(G, v))
-    d_max = max(degrees)
-    max_index = degrees.index(d_max)
-    root_vertex = V[max_index]
-    VL=[root_vertex]
-    init_set = [root_vertex]
-    T = []
-    while len(T)< len(V)-1:
-        relative_degrees = []
-        relative_stars = []
-        for v in init_set:
-            Star = vertex_star(G, v)
-            aux = Star.copy()
-            for e in Star:
-                if e in T:
-                    aux.remove(e)
-                elif (e[0] == v) and (e[1] in VL):
-                    aux.remove(e)
-                elif (e[1] == v) and (e[0] in VL):
-                    aux.remove(e)           
-            relative_degrees.append(len(aux))
-            relative_stars.append(aux)
-        rel_max = max(relative_degrees)
-        rel_index = relative_degrees.index(rel_max)
-        for edge in relative_stars[rel_index]:
-            T.append(edge)
-            if edge[0] == init_set[rel_index]:
-                VL.append(edge[1])
-                init_set.append(edge[1])
-            elif edge[1] == init_set[rel_index]:
-                VL.append(edge[0])
-                init_set.append(edge[0])
-        init_set.remove(init_set[rel_index])
     return T
 
